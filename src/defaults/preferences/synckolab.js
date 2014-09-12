5479ecf2dbb928be56ea2654647b62d43905a08b
Niko <niko.berger@corinis.com>
2014-09-12 16:59:34 +0200

    add default preferences

---
+++ src/defaults/preferences/synckolab.js
@@ -0,0 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+pref("SyncKolab.accounts.list", "");
+pref("SyncKolab.closeWindow", false);
+pref("SyncKolab.configVersion", 0);
+pref("SyncKolab.debugLevel", 0);
+pref("SyncKolab.hideFolder", true);
+pref("SyncKolab.syncOnStart", false);
